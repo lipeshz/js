94352d147672360603f047939a56d8c2c4b536cc
Filipe Gabriel Ferreira de Lima <filipeglima2005@gmail.com>
2024-01-01 18:06:01 -0300

    tipos de dados

--- exe001/js/script.js
+++ exe001/js/script.js
@@ -1,15 +1,20 @@
-// window.alert('Meu primeiro alert!');
-// window.confirm('Está gostando?');
-// window.prompt('Qual seu nome?');
-// Pode ser usado apenas como alert
+/*
+    window.alert('Meu primeiro alert!'); // Exibe um alerta ao usuário
+    window.confirm('Está gostando?'); // Exibe um alerta com botões (confirmar e cancelar)
+    window.prompt('Qual seu nome?'); // Exibe um alerta com um input de texto
+*/
 
-// for(var i = 0; i < 10; i++){
-//     alert(i)
-// }
+// Pode ser usado apenas como alert()
+
+/*
+    for(var i = 0; i < 10; i++){
+        alert(i)
+    }
+*/
 
 document.addEventListener("DOMContentLoaded", function(){ // Executa apenas se o DOM estiver completamente carregado.
     var mensagemHTML = document.getElementById("mensagem"); // Captura o elemento pelo ID no HTML
-    mensagemHTML.innerHTML = "Essa mensagem é um teste"; // E altera sua mensagem final.
+    mensagemHTML.innerHTML = "Essa mensagem é um teste."; // Altera o elemento HTML.
 })
 
 
